757de2fa1e6348abdd1e597a0b70ea04c7190920
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-26 15:42:25 -0300

    fix tickets3

--- src/controllers/ticketsController.js
+++ src/controllers/ticketsController.js
@@ -3,7 +3,7 @@ import { db } from "../db/database.js"
 export async function getTicketDetails(req, res) {
     const {ticketId} = req.params
     try {
-        const result=await db.query(`select tickets.*, cities.city, airlines."airlineName" from tickets join cities on cities.id=tickets."toCityId" join cities on cities.id=tickets."fromCityId" join airlines on airlines.id=tickets."airlineId" where tickets.id=$1;`,[ticketId])
+        const result=await db.query(`select tickets.*, cities.city, airlines."airlineName" from tickets join cities on cities.id=tickets."toCityId" and cities.id=tickets."fromCityId" join airlines on airlines.id=tickets."airlineId" where tickets.id=$1;`,[ticketId])
         return res.status(200).send(result.rows)
         // select tickets.*, cities.city from tickets join cities on cities.id=tickets."toCityId" where tickets."toCityId"=1;
 
@@ -14,7 +14,6 @@ export async function getTicketDetails(req, res) {
 
 export async function getTickets(req, res) {
     const {cityId} = req.params
-
     const {min,max} = req.body
     try {
         const result=await db.query(`select tickets.*, cities.city from tickets join cities on cities.id=tickets."fromCityId" where tickets."toCityId"=$1 and "ticketPrice"<=$2 and "ticketPrice">=$3;`[Number(cityId),max,min])
